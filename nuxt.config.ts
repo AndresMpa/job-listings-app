6d8807808ea2911852c95e309bbdc9d2b5b0ab85
Andres Prieto <andresprieto2703@gmail.com>
2024-11-21 23:33:50 -0500

    ADD: Shadcn & Tailwind
    Adding config files and needs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  $test: { logLevel: "silent" },
-  $development: { logLevel: "warning" },
-  $production: { logLevel: "error" },
-  $env: {
-    staging: { logLevel: "debug" },
-  },
-});
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/i18n'
+  ]
+})
