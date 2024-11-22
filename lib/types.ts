be3c40c1ec6a4ea03fcd516c8b5135800c52cc56
Andres Prieto <andresprieto2703@gmail.com>
2024-11-22 03:02:01 -0500

    ADD: Types
    Tracking types for jobs

---
+++ lib/types.ts
@@ -0,0 +1,17 @@
+interface Job {
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+}
+
+export type { Job };
