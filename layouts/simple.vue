a863ccbf325cd580b40cafee8b0c6bc944b62e10
Andres Prieto <andresprieto2703@gmail.com>
2024-11-22 03:01:02 -0500

    ADD: Simple layout
    Adding a simpler layout

---
+++ layouts/simple.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: "Job Listing App %s",
+});
+</script>
+
+<template>
+  <div class="bg-primary md:bg-theme-desktop bg-theme-mobile">
+    <div class="relative">
+      <div class="absolute top-2 right-2">
+        <AppHoverMenu />
+      </div>
+    </div>
+    <slot />
+  </div>
+</template>
