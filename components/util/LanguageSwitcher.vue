5c97ac6367523e5462ece52048f355049b164567
Andres Prieto <andresprieto2703@gmail.com>
2024-12-05 22:48:58 -0500

    UPDATE: Moving component tp DropDown from shadcn

--- components/util/LanguageSwitcher.vue
+++ components/util/LanguageSwitcher.vue
@@ -1,13 +1,36 @@
 <script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Icon } from "@iconify/vue";
+
 const { setLocale } = useI18n();
 </script>
 
 <template>
-  <div>
-    <div>
-      <button @click="setLocale('en')">en</button>
-      <button @click="setLocale('fr')">fr</button>
-      <p>{{ $t("welcome") }}</p>
-    </div>
-  </div>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Icon
+          icon="radix-icons:chat-bubble"
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <Icon
+          icon="radix-icons:chat-bubble"
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <span class="sr-only">
+          <p>{{ $t("welcome") }}</p>
+        </span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="setLocale('en')"> English </DropdownMenuItem>
+      <DropdownMenuItem @click="setLocale('es')"> Español </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
