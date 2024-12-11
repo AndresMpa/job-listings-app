36c53963dae6a64b7a78d454a2d37091da032c73
Andres Prieto <andresprieto2703@gmail.com>
2024-12-10 23:26:08 -0500

    ADD: Tracking layout

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -6,26 +6,19 @@ useHead({
 
 <template>
   <div class="bg-primary sm:bg-theme-desktop bg-theme-mobile">
-    <div class="relative">
-      <div class="absolute top-2 right-2">
-        <AppHoverMenu />
+    <AppHoverMenu>
+      <div>
+        <UtilLanguageSwitcher />
       </div>
-    </div>
+      <div>
+        <UtilThemeSwitcher />
+      </div>
+    </AppHoverMenu>
     <slot />
   </div>
 </template>
 
 <style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-}
-
 .rotate-enter-active,
 .rotate-leave-active {
   transition: all 0.4s;
