c04f2013691dee32322bee5eeed3de3a1f471c5b
Andres Prieto <andresprieto2703@gmail.com>
2024-12-11 23:07:43 -0500

    ADD: i18n
    Adding needs for i18n

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,5 +1,5 @@
-import english from "#i18n/locales/en.json";
-import spanish from "#i18n/locales/es.json";
+import english from "./i18n/locales/en.json";
+import spanish from "./i18n/locales/es.json";
 
 export default defineI18nConfig(() => ({
   legacy: false,
